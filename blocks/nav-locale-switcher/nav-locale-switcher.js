417d4e3a2d38fca8d72fb3f4487ee8cbb6ecb207
Rachel Waterman <rwaterman@adobe.com>
2025-08-19 20:36:00 +0100

    Compact locale switcher.

--- blocks/nav-locale-switcher/nav-locale-switcher.js
+++ blocks/nav-locale-switcher/nav-locale-switcher.js
@@ -1,4 +1,6 @@
 export default async function decorate(block) {
+  const compactFormat = block.classList.contains('compact');
+
   const links = block.querySelectorAll('a');
   const currentPath = window.location.href;
   links.forEach((link) => {
@@ -6,4 +8,44 @@ export default async function decorate(block) {
       link.setAttribute('data-current', 'true');
     }
   });
+
+  if (compactFormat) {
+    const globeButton = document.createElement('button');
+    const dropdown = document.createElement('div');
+    dropdown.classList.add('nav-locale-switcher__dropdown');
+    dropdown.append(...block.children);
+
+    let timer;
+    let isMouseOver = false;
+    const hideDropdown = () => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        if (!isMouseOver) {
+          block.setAttribute('aria-expanded', 'false');
+        }
+      }, 500);
+    };
+
+    globeButton.addEventListener('click', () => {
+      block.setAttribute('aria-expanded', 'true');
+    });
+    globeButton.addEventListener('mouseover', () => {
+      isMouseOver = true;
+      block.setAttribute('aria-expanded', 'true');
+    });
+    globeButton.addEventListener('mouseleave', () => {
+      isMouseOver = false;
+      hideDropdown();
+    });
+
+    dropdown.addEventListener('mouseover', () => {
+      isMouseOver = true;
+      clearTimeout(timer);
+    });
+    dropdown.addEventListener('mouseleave', () => {
+      isMouseOver = false;
+      hideDropdown();
+    });
+    block.append(globeButton, dropdown);
+  }
 }
