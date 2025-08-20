d5589f14e76ac566098d2b75a69c05d862647a68
Rachel Waterman <rwaterman@adobe.com>
2025-08-20 17:27:51 +0100

    Nav cart dropdown.

--- blocks/nav-cart/nav-cart.js
+++ blocks/nav-cart/nav-cart.js
@@ -1,4 +1,33 @@
-export default function decorate(block) {
+import { fetchPlaceholders } from '../../scripts/aem.js';
+
+export default async function decorate(block) {
+  const placeholders = await fetchPlaceholders();
+
   const cartButton = document.createElement('button');
   block.append(cartButton);
+
+  let timer;
+  const hideDropdown = () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      block.setAttribute('aria-expanded', 'false');
+    }, 500);
+  };
+  const onMouseLeave = () => {
+    hideDropdown();
+    cartButton.removeEventListener('mouseleave', onMouseLeave);
+  };
+
+  cartButton.addEventListener('click', () => {
+    block.setAttribute('aria-expanded', 'true');
+    cartButton.addEventListener('mouseleave', onMouseLeave);
+  });
+
+  const dropdown = document.createElement('div');
+  const dropdownContent = document.createElement('div');
+  dropdown.classList.add('nav-cart__dropdown');
+  dropdownContent.classList.add('nav-cart__dropdown__content');
+  dropdownContent.textContent = placeholders.navCartEmpty;
+  dropdown.append(dropdownContent);
+  block.append(dropdown);
 }
