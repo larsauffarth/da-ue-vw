396b37c13d822e6030665d80fae2d126b61bc646
Rachel Waterman <rwaterman@adobe.com>
2025-08-20 23:07:59 +0100

    Fix header on v small screens, add word break behaviour and scrollable smart links.

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -32,5 +32,60 @@ export default function decorate(block) {
 
     const consumptionWrapperIndex = block.querySelector(':scope > div:nth-child(4)');
     consumptionWrapperIndex.classList.add('hero__consumption-wrapper-index');
+
+    const links = block.querySelector(':scope > div:nth-child(3) p:has(a)');
+    links.classList.add('hero__smart-links');
   }
+
+  const links = block.querySelector('.hero__smart-links');
+  const linksWrapper = document.createElement('div');
+  linksWrapper.classList.add('hero__smart-links-wrapper');
+  links.replaceWith(linksWrapper);
+
+  const linksButtonWrapper = document.createElement('div');
+  linksButtonWrapper.classList.add('hero__smart-links-buttons-wrapper');
+  linksButtonWrapper.append(links);
+  linksWrapper.append(linksButtonWrapper);
+
+  const linksScrollWrapper = document.createElement('div');
+  linksScrollWrapper.classList.add('hero__smart-links-scroll-wrapper');
+  linksScrollWrapper.append(links);
+  linksButtonWrapper.append(linksScrollWrapper);
+
+  const prevButton = document.createElement('button');
+  prevButton.classList.add('hero__smart-links__prev');
+  const nextButton = document.createElement('button');
+  nextButton.classList.add('hero__smart-links__next');
+  linksButtonWrapper.prepend(prevButton);
+  linksButtonWrapper.append(nextButton);
+
+  // FIXME: Scroll behaviour should probably be more sophisticated than this
+  prevButton.addEventListener('click', () => {
+    linksScrollWrapper.scrollLeft -= 100;
+  });
+
+  nextButton.addEventListener('click', () => {
+    linksScrollWrapper.scrollLeft += 100;
+  });
+
+  const checkIfScrollable = () => {
+    if (linksScrollWrapper.scrollLeft > 0) {
+      prevButton.classList.add('visible');
+    } else {
+      prevButton.classList.remove('visible');
+    }
+
+    if (
+      (linksScrollWrapper.scrollWidth - linksScrollWrapper.scrollLeft)
+      > linksScrollWrapper.clientWidth
+    ) {
+      nextButton.classList.add('visible');
+    } else {
+      nextButton.classList.remove('visible');
+    }
+  };
+  const resizeObserver = new ResizeObserver(checkIfScrollable);
+  resizeObserver.observe(linksWrapper);
+
+  linksScrollWrapper.addEventListener('scrollend', checkIfScrollable);
 }
